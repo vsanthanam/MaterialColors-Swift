5b57b8efbc84b3b8f0b17c99d80526938e0a8e5d
Varun Santhanam <talkto@vsanthanam.com>
2021-07-04 07:43:42 -0700

    Update Doc Generation Script

--- gen-docs.sh
+++ gen-docs.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
+rm -rf docs
 jazzy \
     --clean \
     --author "Varun Santhanam" \
     --author_url https://www.vsanthanam.com \
     --swift-build-tool spm \
+    --sdk 
     --build-tool-arguments -Xswiftc,-swift-version,-Xswiftc,5 \
     --module MaterialColors \
     --module-version 0.0.1 \
     --github_url https://www.github.com/vsanthanam/MaterialColors \
     --root-url https://vsanthanam.github.io \
-    --output Documentation \
+    --output docs \
