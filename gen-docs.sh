75fbb0a6031527f83846b7cad90e321af04edbe6
Varun Santhanam <talkto@vsanthanam.com>
2021-10-12 10:19:11 -0700

    Bump version number

--- gen-docs.sh
+++ gen-docs.sh
@@ -8,7 +8,7 @@ jazzy \
     --sdk 
     --build-tool-arguments -Xswiftc,-swift-version,-Xswiftc,5 \
     --module MaterialColors \
-    --module-version 0.0.5 \
+    --module-version 0.0.8 \
     --github_url https://www.github.com/vsanthanam/MaterialColors \
     --root-url https://swiftmaterialcolors.dev \
     --output docs \
